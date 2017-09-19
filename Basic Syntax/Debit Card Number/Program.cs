b3723c738f2afd341f3171378467a766322efd6a
penchev <boyan.penchev@gmail.com>
2017-09-20 02:22:58 +0300

    Debit Card Number program is ready.

---
+++ Basic Syntax/Debit Card Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstInteger = int.Parse(Console.ReadLine());
+            int secondInteger = int.Parse(Console.ReadLine());
+            int thirdInteger = int.Parse(Console.ReadLine());
+            int fourthInteger = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstInteger:D4} {secondInteger:D4} {thirdInteger:D4} {fourthInteger:D4}");
+        }
+    }
+}
