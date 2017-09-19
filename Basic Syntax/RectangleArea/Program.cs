4476b913154e394c248454368b707ec21538058f
penchev <boyan.penchev@gmail.com>
2017-09-20 02:28:12 +0300

    Rectangle Area program is ready.

---
+++ Basic Syntax/RectangleArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{width*height:F2}");
+        }
+    }
+}
