a81b96b79cf64a0a7ecb4e51f595e7ecce2b6723
penchev <boyan.penchev@gmail.com>
2017-09-20 03:13:20 +0300

    BeverageLabels program is ready.

---
+++ Basic Syntax/BeverageLabels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            double energyPer100ml = double.Parse(Console.ReadLine());
+            double sugarPer100ml = double.Parse(Console.ReadLine());
+
+            double energyPerOneMl = energyPer100ml / 100;
+            double energy = energyPerOneMl * volume;
+            double sugarPer1ml = sugarPer100ml / 100;
+            double sugar = sugarPer1ml * volume;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
