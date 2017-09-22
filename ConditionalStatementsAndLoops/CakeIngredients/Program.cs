1c696970db0deb37700052de80433883091fcb88
penchev <boyan.penchev@gmail.com>
2017-09-23 00:37:15 +0300

    Cake Ingredients program is ready.

---
+++ ConditionalStatementsAndLoops/CakeIngredients/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingredient = string.Empty;
+            int number = 0;
+            while ((ingredient= Console.ReadLine()) != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+                number++;
+            } 
+            Console.WriteLine($"Preparing cake with {number} ingredients.");
+        }
+    }
+}
