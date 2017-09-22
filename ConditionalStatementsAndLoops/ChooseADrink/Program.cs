e358402fc04d37a9fc74aa75608915e9e4f10114
penchev <boyan.penchev@gmail.com>
2017-09-22 22:37:03 +0300

    Created new solution and ChooseADrink program is ready.

---
+++ ConditionalStatementsAndLoops/ChooseADrink/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChooseADrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            string perfectDrink = string.Empty;
+
+            switch (profession)
+            {
+                case "Athlete":
+                    perfectDrink = "Water";
+                    break;
+                case "Businessman":
+                case "Businesswoman":
+                    perfectDrink = "Coffee";
+                    break;
+                case "SoftUni Student":
+                    perfectDrink = "Beer";
+                    break;
+                default:
+                    perfectDrink = "Tea";
+                    break;
+
+            }
+
+            Console.WriteLine(perfectDrink);
+        }
+    }
+}
