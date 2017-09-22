e46faeb449cd141cc9606dc7834560b60dc2631f
penchev <boyan.penchev@gmail.com>
2017-09-23 00:30:15 +0300

    Interval of number is ready.

---
+++ ConditionalStatementsAndLoops/IntervalOfNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IntervalOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int buffer = 0;
+
+            if (firstNumber > secondNumber)
+            {
+                buffer = firstNumber;
+                firstNumber = secondNumber;
+                secondNumber = buffer;
+            }
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
