9e59e7ae18c48c8c5693c969a5f029c569769a41
penchev <boyan.penchev@gmail.com>
2017-09-23 00:20:47 +0300

    Word in plural program is ready.

---
+++ ConditionalStatementsAndLoops/WordInPlural/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WordInPlural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            if (word.EndsWith("y"))
+            {
+                word = word.Remove(word.Length - 1);
+                word = word.Insert(word.Length, "ies");
+            } else if (word.EndsWith("ch") || word.EndsWith("o") || word.EndsWith("s") || word.EndsWith("sh")
+                || word.EndsWith("x") || word.EndsWith("z"))
+            {
+                word = word.Insert(word.Length, "es");
+            }
+            else word = word.Insert(word.Length, "s");
+            Console.WriteLine(word);
+        }
+    }
+}
