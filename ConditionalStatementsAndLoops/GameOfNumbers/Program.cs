9a36dad681bd076bfae50a55b03678286c4f080e
penchev <boyan.penchev@gmail.com>
2017-09-23 02:36:01 +0300

    Game of numbers program is ready.

---
+++ ConditionalStatementsAndLoops/GameOfNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GameOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int magicalNumber = int.Parse(Console.ReadLine());
+            int magicNum1 = 0;
+            int magicNum2 = 0;
+            int counter = 0;
+
+            for (int i = N; i <= M; i++)
+            {
+                for (int j = N; j <= M; j++)
+                {
+                    if ((i + j) == magicalNumber)
+                    {
+                        magicNum1 = i;
+                        magicNum2 = j;
+                    }
+                    counter++;
+                }
+            }
+            if (magicNum1 != 0 && magicNum2 != 0)
+                Console.WriteLine($"Number found! {magicNum1} + {magicNum2} = {magicalNumber}");
+            else
+                Console.WriteLine($"{counter} combinations - neither equals {magicalNumber}");
+        }
+    }
+}
