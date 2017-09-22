78c6c24d6808a90f7dc8a3e1093fb210a8fae0c5
penchev <boyan.penchev@gmail.com>
2017-09-23 01:20:35 +0300

    Count the integers program is ready.

---
+++ ConditionalStatementsAndLoops/CountTheIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountTheIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            var input = string.Empty;
+            int buffer = 0;
+            bool flag = false;
+            do
+            {
+                if (flag)
+                    counter++;
+                input = Console.ReadLine();
+                flag = true;
+            } while (int.TryParse(input, out buffer));
+
+            Console.WriteLine(counter);
+        }
+    }
+}
