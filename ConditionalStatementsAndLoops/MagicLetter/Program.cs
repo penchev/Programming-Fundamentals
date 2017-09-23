305e35ee4ca3c8f20917a0d3d93552f61dbe26ad
penchev <boyan.penchev@gmail.com>
2017-09-23 03:16:55 +0300

    Magic letter program is ready.

---
+++ ConditionalStatementsAndLoops/MagicLetter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+            
+            for (char i = firstLetter; i <= secondLetter; i++)
+                for (char j = firstLetter; j <= secondLetter; j++)
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                        if(i!=thirdLetter && j!=thirdLetter && k!=thirdLetter)
+                        Console.Write("{0}{1}{2} ", i, j , k);
+        }
+    }
+}
